ddb9519841d79d50a2bbf9779df56a4369050f1c
imouflih <iliasse.mofloh@gmail.com>
2023-07-03 15:44:24 +0200

    Add logo

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const SiteHeader = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b-2">
       <div className="flex items-center h-16 px-24 space-x-4">
-        <div>logo</div>
+        <div className="flex gap-6">
+          <Link href="/" className="relative flex items-center w-64 h-44">
+            <Image
+              src={"/images/logo.png"}
+              fill
+              className="block object-contain w-full h-full"
+              alt="Logo"
+            />
+          </Link>
+        </div>
         <div>nav menu</div>
         <div className="flex justify-end flex-1 space-x-4">
           <div>social media</div>
