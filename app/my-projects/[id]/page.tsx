e158efef529ff3e04f2b817582df4aff3bb75eff
ILIASSE MOUFLIH <iliasse.mofloh@gmail.com>
2024-01-15 03:09:17 +0100

    Update styling

--- app/my-projects/[id]/page.tsx
+++ app/my-projects/[id]/page.tsx
@@ -12,7 +12,7 @@ const SingleProjectPage: ({
   const project: Project = await getProjectByName(params.id);
 
   return (
-    <div className="flex flex-col justify-center min-h-screen -mt-[68px] overflow-hidden bg-gradient-to-tl from-slate-300 via-slate-400/20 to-slate-300 dark:from-black dark:via-zinc-600/20 dark:to-black">
+    <div className="flex flex-col justify-center min-h-screen -mt-[68px] pt-10 overflow-hidden bg-gradient-to-tl from-slate-300 via-slate-400/20 to-slate-300 dark:from-black dark:via-zinc-600/20 dark:to-black">
       <ParticlesWrapper
         className="absolute inset-0 min-h-screen -z-1"
         quantity={1000}
